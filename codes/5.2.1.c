40048595f0fefbc2f3c13ae76cf8a44f8999fef1
Ayush Tripathi <43269920+Ayush-pbh@users.noreply.github.com>
2021-10-19 13:37:38 +0530

    Add files via upload
    Fixed Compilation Errors

---
+++ codes/5.2.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    int num,sum=0,r,temp;
+    printf("Enter the number:\n ");  
+    scanf("%d",&num);
+    temp=num;           
+    while(temp!=0)
+    {
+        r=temp%10;
+        sum=sum+r;
+        temp=temp/10;
+    }
+    printf("\nGiven number = %d",num);
+    printf("\nSum of the digits = %d",sum);
+}
---
+++ codes/5.2.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+    int n;
+    printf("Input the number 'n'");
+    scanf("%d",&n);
+    float sum=0;
+    for (int i=1;i<=n;i++){
+        sum+=(1/i);
+    }
+    printf("Your Sum is %f", sum);
+}
